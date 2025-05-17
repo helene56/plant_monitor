454cceb3fabb3010d7cd68eb8f465daa914de4c1
helene <helene.maja@hotmail.dk>
2025-05-18 00:34:48 +0200

    add start page for showing water container status

---
+++ lib/pages/water.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyWater extends StatefulWidget {
+  const MyWater({super.key});
+
+  @override
+  State<MyWater> createState() => _MyWaterState();
+}
+
+class _MyWaterState extends State<MyWater> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('my text');
+  }
+}
