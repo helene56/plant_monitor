0a7fb0e2960d36787041f63a5a4799ffd1b8a54d
helene <helene.maja@hotmail.dk>
2025-05-18 20:58:27 +0200

    add floating 'add' button

--- lib/main.dart
+++ lib/main.dart
@@ -15,41 +15,78 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   int currentPageindex = 1;
 
   final List<Widget> widgetOptions = [
-      MyWater(),
-      MyHome(),
-      MyStats()
-    ];
+    MyWater(),
+    MyHome(),
+    MyStats(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        floatingActionButton:
+            FloatingActionButton.small(
+              onPressed: () {
+                // Add your onPressed code here!
+              },
+              backgroundColor: Colors.lightGreen,
+              shape: CircleBorder(),
+              child: const Icon(Icons.add),
+            ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         bottomNavigationBar: Padding(
-          padding: const EdgeInsets.only(bottom: 8, left: 10, right: 10),
+          padding: const EdgeInsets.only(
+            bottom: 8,
+            left: 10,
+            right: 10,
+          ),
           child: ClipRRect(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(35), bottom: Radius.circular(35)),
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(35),
+              bottom: Radius.circular(35),
+            ),
             child: NavigationBar(
               selectedIndex: currentPageindex,
-              backgroundColor: Color.fromARGB(255, 250, 229, 212),
-              indicatorColor: Color.fromARGB(255, 250, 229, 212),
+              backgroundColor: Color.fromARGB(
+                255,
+                250,
+                229,
+                212,
+              ),
+              indicatorColor: Color.fromARGB(
+                255,
+                250,
+                229,
+                212,
+              ),
               destinations: const <Widget>[
                 NavigationDestination(
-                  selectedIcon: Icon(Icons.local_drink, color: Colors.blueAccent,),
-                  icon: Icon(Icons.local_drink_outlined),
+                  selectedIcon: Icon(
+                    Icons.local_drink,
+                    color: Colors.blueAccent,
+                  ),
+                  icon: Icon(
+                    Icons.local_drink_outlined,
+                  ),
                   label: 'Vand beholder',
                 ),
                 NavigationDestination(
-                  selectedIcon: Icon(Icons.eco, color: Colors.green,),
+                  selectedIcon: Icon(
+                    Icons.eco,
+                    color: Colors.green,
+                  ),
                   icon: Icon(Icons.eco_outlined),
                   label: 'Planter',
                 ),
                 NavigationDestination(
-                  selectedIcon: Icon(Icons.straighten, color: Colors.redAccent,),
+                  selectedIcon: Icon(
+                    Icons.straighten,
+                    color: Colors.redAccent,
+                  ),
                   icon: Icon(Icons.straighten),
                   label: 'Statistik',
                 ),
@@ -63,7 +100,7 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
         backgroundColor: Color(0xfff6f9f8),
-        body:  widgetOptions[currentPageindex],
+        body: widgetOptions[currentPageindex],
       ),
     );
   }
