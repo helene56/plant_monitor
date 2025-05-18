c1c561c876b720b002c2b35438b987ef483cf49c
helene <helene.maja@hotmail.dk>
2025-05-18 21:23:17 +0200

    new page for plant state

---
+++ lib/pages/plant_stats.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyPlantStat extends StatelessWidget {
+  const MyPlantStat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('plant stats!!!')),
+    );
+  }
+}
