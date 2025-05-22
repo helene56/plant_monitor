7006192ca6efe4c5f93eaa2534cb648da8d9cc9f
helene <helene.maja@hotmail.dk>
2025-05-22 21:29:50 +0200

    add mousture, sun, water, air temp and earth temp level indicators.

--- lib/pages/plant_stats.dart
+++ lib/pages/plant_stats.dart
@@ -12,6 +12,7 @@ class MyPlantStat extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Row(
             children: [
@@ -21,13 +22,116 @@ class MyPlantStat extends StatelessWidget {
                   './images/plant_test.png',
                 ),
               ),
-              Text('title', style: TextStyle(fontSize: 24),),
+              Text(
+                'title',
+                style: TextStyle(fontSize: 24),
+              ),
             ],
           ),
           Center(
             child: Text(
               'plant stats!!! id: $plantId',
             ),
+          ),
+          Text('Vand'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.water_drop, color: const Color.fromARGB(255, 105, 190, 240),),
+              SizedBox(width: 10,),
+              SizedBox(
+                width: 260,
+                child: LinearProgressIndicator(
+                  backgroundColor: const Color.fromARGB(109, 105, 190, 240),
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ),
+                  color: const Color.fromARGB(255, 105, 190, 240),
+                  minHeight: 20,
+                  value: 0.5,
+                ),
+              ),
+            ],
+          ),
+          Text('Sollys'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.wb_sunny, color: Colors.amber,),
+              SizedBox(width: 10,),
+              SizedBox(
+                width: 260,
+                child: LinearProgressIndicator(
+                  backgroundColor: const Color.fromARGB(109, 240, 229, 105),
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ),
+                  color: const Color.fromARGB(255, 240, 229, 105),
+                  minHeight: 20,
+                  value: 0.5,
+                ),
+              ),
+            ],
+          ),
+          Text('Fugt'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.foggy, color: const Color.fromARGB(255, 33, 150, 243),),
+              SizedBox(width: 10,),
+              SizedBox(
+                width: 260,
+                child: LinearProgressIndicator(
+                  backgroundColor: const Color.fromARGB(109, 33, 150, 243),
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ),
+                  color: const Color.fromARGB(255, 33, 150, 243),
+                  minHeight: 20,
+                  value: 0.5,
+                ),
+              ),
+            ],
+          ),
+          Text('Luft temperatur'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.thermostat, color: const Color.fromARGB(255, 255, 112, 67),),
+              SizedBox(width: 10,),
+              SizedBox(
+                width: 260,
+                child: LinearProgressIndicator(
+                  backgroundColor: const Color.fromARGB(109, 255, 112, 67),
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ),
+                  color: const Color.fromARGB(255, 255, 112, 67),
+                  minHeight: 20,
+                  value: 0.5,
+                ),
+              ),
+            ],
+          ),
+          Text('Jord temperatur'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.thermostat, color: const Color.fromARGB(255, 141, 110, 99),),
+              SizedBox(width: 10,),
+              SizedBox(
+                width: 260,
+                child: LinearProgressIndicator(
+                  backgroundColor: const Color.fromARGB(109, 141, 110, 99),
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ),
+                  color: const Color.fromARGB(255, 141, 110, 99),
+                  minHeight: 20,
+                  value: 0.5,
+                ),
+              ),
+            ],
           ),
         ],
       ),
