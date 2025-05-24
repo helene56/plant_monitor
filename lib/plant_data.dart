c65eb9c40e45d8ddf2811895e9e03858d541a3e7
helene <helene.maja@hotmail.dk>
2025-05-24 23:05:12 +0200

    rename variable

--- lib/plant_data.dart
+++ lib/plant_data.dart
@@ -4,13 +4,13 @@ class Plant {
   final String type;
   final String waterNeeds;
   final List<int> sunLux;
-  final List<int> temp;
+  final List<int> airTemp;
   final List<int> humidity;
 
-  Plant({required this.name, required this.type, required this.waterNeeds, required this.sunLux, required this.temp, required this.humidity});
+  Plant({required this.name, required this.type, required this.waterNeeds, required this.sunLux, required this.airTemp, required this.humidity});
 }
 
 // note: in case of high humidity the plant will need less water.
 List<Plant> plants = [
-  Plant(name: 'Gummi træ', type: 'ficus elastica', waterNeeds: 'Medium', sunLux: [250, 1000], temp: [18, 27], humidity: [50, 80]),
+  Plant(name: 'Gummi træ', type: 'ficus elastica', waterNeeds: 'Medium', sunLux: [250, 1000], airTemp: [18, 27], humidity: [50, 80]),
 ];
