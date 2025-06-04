53bf50b0302a86e24dd9f493990191f75dca49f0
helene <helene.maja@hotmail.dk>
2025-06-04 17:34:15 +0200

    add bluetooth permissions and file

---
+++ lib/bluetooth_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+// TODO: 
+// 1. start scanning for devices when add floating button is clicked
+// 2. all devices with certain id (to be determined later) should be showed in the list availible 
+//    to choose in choice chip select
+// 3. connect to device that has been selected
