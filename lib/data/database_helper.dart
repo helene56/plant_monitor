fc77aabe752b4044186fb4e61cfe394ff6bbe046
helene <helene.maja@hotmail.dk>
2025-06-23 23:17:54 +0200

    do multiple db.exectute as a single does not work for mulitple tables. add function to get the sensors with different containers.

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
+import 'package:plant_monitor/data/plant_container.dart';
 import 'package:plant_monitor/data/plant_sensor_data.dart';
+import 'package:plant_monitor/data/water_container.dart';
 import 'package:sqflite/sqflite.dart';
 import 'plant.dart';
 import 'plant_type.dart';
@@ -95,7 +97,41 @@ Future<PlantSensorData> getSensor(Database database, int id) async {
   ].first;
 }
 
-Future<List<PlantSensorData>> getSensors(Database database) async {
+Future<List<String>> getSelectedSensors(
+  Database database,
+  List<PlantSensorData> sensors,
+) async {
+  final db = database;
+
+  List<int> waterContainerId = [];
+  List<String> selectedSensorsId = [];
+  for (var sensor in sensors) {
+    final List<Map<String, Object?>> plantContainerMap = await db.query(
+      'plant_containers',
+      where: 'plantId = ?',
+      // Pass the plant sensor's id as a whereArg to prevent SQL injection.
+      whereArgs: [sensor.id],
+    );
+
+    var plantContainers = [
+      for (var {'plantId': id as int, 'containerId': containerId as int}
+          in plantContainerMap)
+        PlantContainer(plantId: id, containerId: containerId),
+    ];
+    var plantyplants = await allPlants(db);
+    var containyPlants = await allPlantContainers(db);
+    for (var plantContainer in plantContainers) {
+      if (!waterContainerId.contains(plantContainer.containerId)) {
+        waterContainerId.add(plantContainer.containerId);
+        selectedSensorsId.add(sensor.sensorId);
+      }
+    }
+  }
+
+  return selectedSensorsId;
+}
+
+Future<List<PlantSensorData>> getAllSensors(Database database) async {
   // Get a reference to the database.
   final db = database;
 
@@ -169,6 +205,28 @@ Future<List<Plant>> allPlants(Database database) async {
   ];
 }
 
+Future<List<PlantContainer>> allPlantContainers(Database database) async {
+  // Get a reference to the database.
+  final db = database;
+  // Query the table for all the plants.
+  final List<Map<String, Object?>> plantMapContainers = await db.query('plant_containers');
+
+  // Convert the list of each plant's fields into a list of `Plant` objects.
+  return [
+    for (final {
+          'plantId': plantId as int,
+          'containerId': containerId as int,
+        }
+        in plantMapContainers)
+      PlantContainer(
+        plantId: plantId,
+        containerId: containerId,
+      ),
+  ];
+}
+
+
+
 Future<void> updateRecord(
   Database database,
   String table,
@@ -194,71 +252,69 @@ Future<void> deleteRecord(Database database, String table, int id) async {
   );
 }
 
-
 Future<void> getLatestEntry(Database database, String table) async {
   final db = database;
   await db.query(table, orderBy: 'id DESC', limit: 1);
 }
-
 
 Future<Database> initializeDatabase() async {
   // Avoid errors caused by flutter upgrade.
   // Importing 'package:flutter/widgets.dart' is required.
   WidgetsFlutterBinding.ensureInitialized();
 
-  String sql = '''
-    CREATE TABLE plants(
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      type TEXT,
-      waterNeedsMin INTEGER,
-      waterNeedsMax INTEGER,
-      sunLuxMin INTEGER,
-      sunLuxMax INTEGER,
-      airTempMin INTEGER,
-      airTempMax INTEGER,
-      humidityMin INTEGER,
-      humidityMax INTEGER
-    );
-    CREATE TABLE plant_types(
-      id INTEGER PRIMARY KEY,
-      label TEXT,
-      type TEXT,
-      waterNeedsMin INTEGER,
-      waterNeedsMax INTEGER,
-      sunLuxMin INTEGER,
-      sunLuxMax INTEGER,
-      airTempMin INTEGER,
-      airTempMax INTEGER,
-      humidityMin INTEGER,
-      humidityMax INTEGER
-    );
-    CREATE TABLE plant_sensor(
-      id INTEGER PRIMARY KEY,
-      sensorId TEXT,
-      sensorName TEXT,
-      water INTEGER,
-      sunLux INTEGER,
-      airTemp INTEGER,
-      earthTemp INTEGER,
-      humidity INTEGER
-    );
-    CREATE TABLE containers(
-      id INTEGER PRIMARY KEY,
-      current_water_level REAL,
-    );
-    CREATE TABLE plant_containers(
-      id INTEGER PRIMARY KEY,
-      plant_id INTEGER,
-      container_id INTEGER,
-      FOREIGN KEY (plant_id) REFERENCES plants (plant_id)
-        ON UPDATE SET NULL
-        ON DELETE SET NULL,
-      FOREIGN KEY (container_id) REFERENCES containers (container_id)
-        ON UPDATE SET NULL
-        ON DELETE SET NULL,
-    );
-  ''';
+  // String sql = '''
+  //   CREATE TABLE plants(
+  //     id INTEGER PRIMARY KEY,
+  //     name TEXT,
+  //     type TEXT,
+  //     waterNeedsMin INTEGER,
+  //     waterNeedsMax INTEGER,
+  //     sunLuxMin INTEGER,
+  //     sunLuxMax INTEGER,
+  //     airTempMin INTEGER,
+  //     airTempMax INTEGER,
+  //     humidityMin INTEGER,
+  //     humidityMax INTEGER
+  //   );
+  //   CREATE TABLE plant_types(
+  //     id INTEGER PRIMARY KEY,
+  //     label TEXT,
+  //     type TEXT,
+  //     waterNeedsMin INTEGER,
+  //     waterNeedsMax INTEGER,
+  //     sunLuxMin INTEGER,
+  //     sunLuxMax INTEGER,
+  //     airTempMin INTEGER,
+  //     airTempMax INTEGER,
+  //     humidityMin INTEGER,
+  //     humidityMax INTEGER
+  //   );
+  //   CREATE TABLE plant_sensor(
+  //     id INTEGER PRIMARY KEY,
+  //     sensorId TEXT,
+  //     sensorName TEXT,
+  //     water INTEGER,
+  //     sunLux INTEGER,
+  //     airTemp INTEGER,
+  //     earthTemp INTEGER,
+  //     humidity INTEGER
+  //   );
+  //   CREATE TABLE containers(
+  //     id INTEGER PRIMARY KEY,
+  //     current_water_level REAL,
+  //   );
+  //   CREATE TABLE plant_containers(
+  //     id INTEGER PRIMARY KEY,
+  //     plant_id INTEGER,
+  //     container_id INTEGER,
+  //     FOREIGN KEY (plant_id) REFERENCES plants (plant_id)
+  //       ON UPDATE SET NULL
+  //       ON DELETE SET NULL,
+  //     FOREIGN KEY (container_id) REFERENCES containers (container_id)
+  //       ON UPDATE SET NULL
+  //       ON DELETE SET NULL,
+  //   );
+  // ''';
 
   // Open the database and store the reference.
   final database = await openDatabase(
@@ -266,51 +322,72 @@ Future<Database> initializeDatabase() async {
     // When the database is first created, create a table to store plants.
     onCreate: (db, version) {
       // Run the CREATE TABLE statement on the database.
-      db.execute(sql);
-      // db.execute(
-      //   'CREATE TABLE plants('
-      //   'id INTEGER PRIMARY KEY, '
-      //   'name TEXT, '
-      //   'type TEXT, '
-      //   'waterNeedsMin INTEGER, '
-      //   'waterNeedsMax INTEGER, '
-      //   'sunLuxMin INTEGER, '
-      //   'sunLuxMax INTEGER, '
-      //   'airTempMin INTEGER, '
-      //   'airTempMax INTEGER, '
-      //   'humidityMin INTEGER, '
-      //   'humidityMax INTEGER'
-      //   ')',
-      // );
+      // db.execute(sql);
+      db.execute(
+        'CREATE TABLE plants('
+        'id INTEGER PRIMARY KEY, '
+        'name TEXT, '
+        'type TEXT, '
+        'waterNeedsMin INTEGER, '
+        'waterNeedsMax INTEGER, '
+        'sunLuxMin INTEGER, '
+        'sunLuxMax INTEGER, '
+        'airTempMin INTEGER, '
+        'airTempMax INTEGER, '
+        'humidityMin INTEGER, '
+        'humidityMax INTEGER'
+        ')',
+      );
 
-      // db.execute(
-      //   'CREATE TABLE plant_types('
-      //   'id INTEGER PRIMARY KEY,'
-      //   'label TEXT,'
-      //   'type TEXT, '
-      //   'waterNeedsMin INTEGER, '
-      //   'waterNeedsMax INTEGER, '
-      //   'sunLuxMin INTEGER, '
-      //   'sunLuxMax INTEGER, '
-      //   'airTempMin INTEGER, '
-      //   'airTempMax INTEGER, '
-      //   'humidityMin INTEGER, '
-      //   'humidityMax INTEGER'
-      //   ')',
-      // );
+      db.execute(
+        'CREATE TABLE plant_types('
+        'id INTEGER PRIMARY KEY,'
+        'label TEXT,'
+        'type TEXT, '
+        'waterNeedsMin INTEGER, '
+        'waterNeedsMax INTEGER, '
+        'sunLuxMin INTEGER, '
+        'sunLuxMax INTEGER, '
+        'airTempMin INTEGER, '
+        'airTempMax INTEGER, '
+        'humidityMin INTEGER, '
+        'humidityMax INTEGER'
+        ')',
+      );
 
-      // db.execute(
-      //   'CREATE TABLE plant_sensor('
-      //   'id INTEGER PRIMARY KEY,'
-      //   'sensorId TEXT, '
-      //   'sensorName TEXT, '
-      //   'water INTEGER, '
-      //   'sunLux INTEGER, '
-      //   'airTemp INTEGER, '
-      //   'earthTemp INTEGER, '
-      //   'humidity INTEGER '
-      //   ')',
-      // );
+      db.execute(
+        'CREATE TABLE plant_sensor('
+        'id INTEGER PRIMARY KEY,'
+        'sensorId TEXT, '
+        'sensorName TEXT, '
+        'water INTEGER, '
+        'sunLux INTEGER, '
+        'airTemp INTEGER, '
+        'earthTemp INTEGER, '
+        'humidity INTEGER '
+        ')',
+      );
+
+      // Add containers table
+      db.execute(
+        'CREATE TABLE containers('
+        'id INTEGER PRIMARY KEY,'
+        'currentWaterLevel REAL'
+        ')',
+      );
+
+      // Add plant_containers table
+      db.execute(
+        'CREATE TABLE plant_containers('
+        'id INTEGER PRIMARY KEY,'
+        'plantId INTEGER,'
+        'containerId INTEGER,'
+        'FOREIGN KEY (plantId) REFERENCES plants(id) '
+        'ON UPDATE SET NULL ON DELETE SET NULL,'
+        'FOREIGN KEY (containerId) REFERENCES containers(id) '
+        'ON UPDATE SET NULL ON DELETE SET NULL'
+        ')',
+      );
     },
     version: 1,
   );
