904bdbc0786eb4fd5b5c5e79dd3d54a98e882e40
helene <helene.maja@hotmail.dk>
2025-06-25 23:42:40 +0200

    add the wheel with water content to water page, remove from statistics page

--- lib/pages/statistics.dart
+++ lib/pages/statistics.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+
 
 class MyStats extends StatefulWidget {
   const MyStats({super.key});
@@ -12,102 +12,8 @@ class _MyStatsState extends State<MyStats> {
   @override
   // TODO: move to water page
   Widget build(BuildContext context) {
-    return Center(child: CustomCircleIcons());
+    return Center(child: Text('this is the statitistics'));
   }
 }
 
-class CustomCircleIcons extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Configuration
-    final double outerRadius = 100;
-    final double iconRadius = 24;
-    final double iconCircleRadius = outerRadius * 0.8;
-    final List<IconData> icons = [
-      Icons.home,
-      Icons.favorite,
-      Icons.settings,
-      Icons.person,
-      Icons.lightbulb,
-      Icons.abc,
-    ];
 
-    return Center(
-      child: SizedBox(
-        width: outerRadius * 2,
-        height: outerRadius * 2,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Outer circle visual (optional)
-            Container(
-              width: outerRadius * 2,
-              height: outerRadius * 2,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.blue[50]!, width: 40),
-              ),
-            ),
-            // Center icon
-            Container(
-              width: iconRadius * 2 * 2,
-              height: iconRadius * 2 * 2,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [BoxShadow(blurRadius: 4, color: Colors.black12)],
-              ),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  SizedBox(
-                    width: 90,
-                    height: 90,
-                    child: CircularProgressIndicator(
-                      value: 0.5, // 50% fill
-                      strokeWidth: 8,
-                      backgroundColor: Colors.blue[50],
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                    ),
-                  ),
-                  Icon(Icons.water_drop, size: 70, color: Colors.blue),
-                  Positioned(
-                    top: 30,
-                    child: Text(
-                      '50%',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            // Icons around the circle
-            ...List.generate(icons.length, (i) {
-              final double angle = (2 * pi / icons.length) * i;
-              final double x = iconCircleRadius * cos(angle);
-              final double y = iconCircleRadius * sin(angle);
-              return Positioned(
-                left: outerRadius + x - iconRadius,
-                top: outerRadius + y - iconRadius,
-                child: Container(
-                  width: iconRadius * 2,
-                  height: iconRadius * 2,
-                  child: Tooltip(
-                    message: 'hey',
-                    triggerMode: TooltipTriggerMode.tap,
-                    preferBelow: false,
-                    child: Icon(icons[i], size: 28, color: Colors.blueAccent),
-                  ),
-                ),
-              );
-            }),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/pages/water.dart
+++ lib/pages/water.dart
@@ -7,16 +7,17 @@ import 'package:plant_monitor/data/database_helper.dart';
 import 'package:plant_monitor/data/water_container.dart';
 import 'package:plant_monitor/main.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:math';
 
 class MyWater extends ConsumerStatefulWidget {
   final List<Plant> plantCards;
   const MyWater({super.key, required this.plantCards});
 
   @override
-  ConsumerState<MyWater> createState() => _MyWaterState();
+  ConsumerState<MyWater> createState() => _MywaterFill();
 }
 
-class _MyWaterState extends ConsumerState<MyWater> {
+class _MywaterFill extends ConsumerState<MyWater> {
   PlantSensorData? plantSensor;
   List<int> statuses = [];
 
@@ -33,8 +34,7 @@ class _MyWaterState extends ConsumerState<MyWater> {
     List<int> newStatuses = [];
     final db = ref.read(appDatabase);
     List<WaterContainer> waterContainers = await getAllWaterContainers(db);
-    for (var container in waterContainers)
-    {
+    for (var container in waterContainers) {
       newStatuses.add(container.currentWaterLevel);
     }
     setState(() {
@@ -42,21 +42,19 @@ class _MyWaterState extends ConsumerState<MyWater> {
     });
   }
 
-
   void initializeSensor() async {
     List<int> newStatuses = [];
     final db = ref.read(appDatabase);
-    
+
     List<PlantSensorData> allSensors = await getAllSensors(db);
     List<String> selectSensors = await getSelectedSensors(db, allSensors);
-  
+
     for (var sensorId in selectSensors) {
       final int? status = await subscibeGetPumpStatus(
         BluetoothDevice.fromId(sensorId),
         db,
       );
-      if (status == -1)
-      {
+      if (status == -1) {
         lastKnownPumpStatus();
         return;
       }
@@ -66,31 +64,22 @@ class _MyWaterState extends ConsumerState<MyWater> {
     setState(() {
       statuses = newStatuses;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: SizedBox(
-        height: 300,
+        height: 697.4,
         child: ListView.builder(
-          scrollDirection: Axis.horizontal,
+          // scrollDirection: Axis.horizontal,
           padding: const EdgeInsets.all(8),
           itemCount: statuses.length,
           itemBuilder: (BuildContext context, int index) {
-            int itemColorValue = 200 + (index * 200);
-            // Clamp the value to the valid range for Colors.amber (100–900)
-            itemColorValue = itemColorValue.clamp(100, 900);
-        
             return Container(
-              margin: EdgeInsets.all(10),
-              height: 50,
-              color: Colors.amber[itemColorValue],
+              margin: EdgeInsets.all(15),
               child: Center(
-                child: Text(
-                  'my text for water container. This is the stat: ${statuses[index]}',
-                ),
+                child: CustomCircleIcons(waterFill: statuses[index]),
               ),
             );
           },
@@ -100,7 +89,101 @@ class _MyWaterState extends ConsumerState<MyWater> {
   }
 }
 
+class CustomCircleIcons extends StatelessWidget {
+  final int waterFill;
+  const CustomCircleIcons({super.key, required this.waterFill});
 
-// Center(
-//       child: Text('my text for water container. This is the stat: $pump'),
-//     );
+  @override
+  Widget build(BuildContext context) {
+    // Configuration
+    final double outerRadius = 100;
+    final double iconRadius = 24;
+    final double iconCircleRadius = outerRadius * 0.8;
+    final List<IconData> icons = [
+      Icons.home,
+      Icons.favorite,
+      Icons.settings,
+      Icons.person,
+      Icons.lightbulb,
+      Icons.abc,
+    ];
+
+    return Center(
+      child: SizedBox(
+        width: outerRadius * 2,
+        height: outerRadius * 2,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // Outer circle visual (optional)
+            Container(
+              width: outerRadius * 2,
+              height: outerRadius * 2,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.blue[50]!, width: 40),
+              ),
+            ),
+            // Center icon
+            Container(
+              width: iconRadius * 2 * 2,
+              height: iconRadius * 2 * 2,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [BoxShadow(blurRadius: 4, color: Colors.black12)],
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
+                    width: 90,
+                    height: 90,
+                    child: CircularProgressIndicator(
+                      value: waterFill / 100, // 50% fill
+                      strokeWidth: 8,
+                      backgroundColor: Colors.blue[50],
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                    ),
+                  ),
+                  Icon(Icons.water_drop, size: 70, color: Colors.blue),
+                  Positioned(
+                    top: 30,
+                    child: Text(
+                      '$waterFill%',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Icons around the circle
+            ...List.generate(icons.length, (i) {
+              final double angle = (2 * pi / icons.length) * i;
+              final double x = iconCircleRadius * cos(angle);
+              final double y = iconCircleRadius * sin(angle);
+              return Positioned(
+                left: outerRadius + x - iconRadius,
+                top: outerRadius + y - iconRadius,
+                child: Container(
+                  width: iconRadius * 2,
+                  height: iconRadius * 2,
+                  child: Tooltip(
+                    message: 'hey',
+                    triggerMode: TooltipTriggerMode.tap,
+                    preferBelow: false,
+                    child: Icon(icons[i], size: 28, color: Colors.blueAccent),
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
