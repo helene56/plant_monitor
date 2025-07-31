675c5ea5a9e6daeafac2aa4171081cf7314679c5
helene <helene.maja@hotmail.dk>
2025-07-31 23:31:54 +0200

    add ids for commands to send to sensor

---
+++ lib/data/sensor_cmd_id.dart
@@ -0,0 +1,7 @@
+class SensorCmdId {
+  static const int temperature = 0;
+  static const int humidity = 1;
+  static const int light = 2;
+  static const int soil = 3;
+  static const int pump = 4;
+}
