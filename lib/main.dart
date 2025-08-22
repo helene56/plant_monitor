8704760b609a986d8a5329250c2702328aba36f7
helene <helene.maja@hotmail.dk>
2025-08-22 17:24:21 +0200

    hide navigationbar and floating button when in landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -96,55 +96,67 @@ class _MyAppState extends ConsumerState<MyApp> {
       MyHome(plantsCards: plantsCards),
       MyStats(),
     ];
-    return Scaffold(
-      floatingActionButton: FloatingActionButton.small(
-        onPressed: () {
-          // Add your onPressed code here!
-          _openDialog();
-        },
-        backgroundColor: Colors.lightGreen,
-        shape: CircleBorder(),
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 8, left: 10, right: 10),
-        child: ClipRRect(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(35),
-            bottom: Radius.circular(35),
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        final bool isLandscape = orientation == Orientation.landscape;
+
+        return Scaffold(
+          floatingActionButton: Visibility(
+            visible: !isLandscape, // The button is visible only in portrait mode
+            child: FloatingActionButton.small(
+              onPressed: () {
+                _openDialog();
+              },
+              backgroundColor: Colors.lightGreen,
+              shape: const CircleBorder(),
+              child: const Icon(Icons.add),
+            ),
           ),
-          child: NavigationBar(
-            selectedIndex: currentPageindex,
-            backgroundColor: Color.fromARGB(255, 250, 229, 212),
-            indicatorColor: Color.fromARGB(255, 250, 229, 212),
-            destinations: const <Widget>[
-              NavigationDestination(
-                selectedIcon: Icon(Icons.local_drink, color: Colors.blueAccent),
-                icon: Icon(Icons.local_drink_outlined),
-                label: 'Vand beholder',
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+          bottomNavigationBar: Visibility(
+            visible: !isLandscape, // The navigation bar is visible only in portrait mode
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8, left: 10, right: 10),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(35),
+                  bottom: Radius.circular(35),
+                ),
+                child: NavigationBar(
+                  selectedIndex: currentPageindex,
+                  backgroundColor: const Color.fromARGB(255, 250, 229, 212),
+                  indicatorColor: const Color.fromARGB(255, 250, 229, 212),
+                  destinations: const <Widget>[
+                    NavigationDestination(
+                      selectedIcon: Icon(Icons.local_drink, color: Colors.blueAccent),
+                      icon: Icon(Icons.local_drink_outlined),
+                      label: 'Vand beholder',
+                    ),
+                    NavigationDestination(
+                      selectedIcon: Icon(Icons.eco, color: Colors.green),
+                      icon: Icon(Icons.eco_outlined),
+                      label: 'Planter',
+                    ),
+                    NavigationDestination(
+                      selectedIcon: Icon(Icons.straighten, color: Colors.redAccent),
+                      icon: Icon(Icons.straighten),
+                      label: 'Statistik',
+                    ),
+                  ],
+                  onDestinationSelected: (int index) {
+                    setState(() {
+                      currentPageindex = index;
+                    });
+                  },
+                ),
               ),
-              NavigationDestination(
-                selectedIcon: Icon(Icons.eco, color: Colors.green),
-                icon: Icon(Icons.eco_outlined),
-                label: 'Planter',
-              ),
-              NavigationDestination(
-                selectedIcon: Icon(Icons.straighten, color: Colors.redAccent),
-                icon: Icon(Icons.straighten),
-                label: 'Statistik',
-              ),
-            ],
-            onDestinationSelected: (int index) {
-              setState(() {
-                currentPageindex = index;
-              });
-            },
+            ),
           ),
-        ),
-      ),
-      backgroundColor: Color(0xfff6f9f8),
-      body: widgetOptions[currentPageindex],
+          backgroundColor: const Color(0xfff6f9f8),
+          body: widgetOptions[currentPageindex],
+        );
+      },
     );
   }
 }
