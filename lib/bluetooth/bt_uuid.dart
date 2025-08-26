5c207de260e6d259edd55d63de297e52b1d655a5
helene <helene.maja@hotmail.dk>
2025-08-27 00:28:15 +0200

    add bluetooth ids

---
+++ lib/bluetooth/bt_uuid.dart
@@ -0,0 +1,7 @@
+class BtUuid {
+  static const String serviceId = "0f956141-6b9c-4a41-a6df-977ac4b99d78";
+  static const String calibrationCharId = "0f956145-6b9c-4a41-a6df-977ac4b99d78";
+  static const String sensorCmdCharId = "0f956144-6b9c-4a41-a6df-977ac4b99d78";
+  static const String sensorLogCharId = "0f956146-6b9c-4a41-a6df-977ac4b99d78"; 
+
+}
