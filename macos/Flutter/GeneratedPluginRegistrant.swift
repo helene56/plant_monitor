c84314666dd8a2035731d820fce0d8bf19c6ea43
helene <helene.maja@hotmail.dk>
2025-10-01 14:05:56 +0200

    generated changes

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_blue_plus_darwin
-import flutter_calendar_heatmap
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
-  FlutterCalendarHeatmapPlugin.register(with: registry.registrar(forPlugin: "FlutterCalendarHeatmapPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_calendar_heatmap/flutter_calendar_heatmap_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterCalendarHeatmapPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterCalendarHeatmapPluginCApi"));
 }
